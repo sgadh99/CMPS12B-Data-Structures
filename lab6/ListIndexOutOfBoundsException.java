6e11ddec2e1ff96e32eb0d20b32172f3d6e756b0
sgadh99 <50816245+sgadh99@users.noreply.github.com>
2019-05-19 23:50:19 -0700

    Delete ListIndexOutOfBoundsException.java

--- lab6/ListIndexOutOfBoundsException.java
+++
@@ -1,7 +0,0 @@
-
-@SuppressWarnings("serial")
-public class ListIndexOutOfBoundsException extends IndexOutOfBoundsException{
-   public ListIndexOutOfBoundsException(String s){
-      super(s);
-   }
-}
